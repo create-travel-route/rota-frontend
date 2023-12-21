101d76c4cfd41894bd2f5c27c8b47733f8a7e628
seyma <seyma.6ok@hotmail.com>
2023-12-21 17:40:32 +0300

    buttons

---
+++ src/Components/Button/TertiaryButton.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function TertiaryButton() {
+  return (
+    <div>
+      <Button className="tertiaryButton">TertiaryButton</Button>
+    </div>
+  );
+}
--- src/Pages/Homepage/index.jsx
+++ src/Pages/Homepage/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { Input } from '../../Components';
+import MainButton from '../../Components/Button/MainButton';
+import SecondaryButton from '../../Components/Button/SecondaryButton';
+import TertiaryButton from '../../Components/Button/TertiaryButton';
 
 function Homepage() {
   return (
     <Box sx={{ flexGrow: 1, m: 2 }}>
       <Input label="beyza" />
+      <MainButton></MainButton>
+      <SecondaryButton></SecondaryButton>
+      <TertiaryButton></TertiaryButton>
     </Box>
   );
 }
