daafc628ff16e6b23c4ed9f8cf5c092217027ecf
aysegulgencel <aysegulgencel0@gmail.com>
2024-05-17 12:19:49 +0300

    property list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './Layout/Layout';
-import { Homepage } from './Pages';
+import { Homepage, PropertyList } from './Pages';
 import { routes } from './Routes/Routes';
 import Control from './Routes/Control';
 const App = () => {
@@ -9,6 +9,7 @@ const App = () => {
     <Layout>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/property-list" element={<PropertyList />} />
         {routes.map(({ path, element: Element, permissions, params }, index) => (
           <Route key={index} element={<Control permissions={permissions} />}>
             <Route key={index} path={path} element={<Element {...params} />} />
---
+++ src/Components/Filter/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Paper, Box, Typography } from '@mui/material';
+
+const Filter = ({ children, title }) => {
+  return (
+    <Paper
+      sx={{
+        padding: 3,
+        elevation: 2,
+        mb: 2
+      }}>
+      <Typography
+        sx={{
+          color: 'header.main',
+          fontWeight: 600,
+          textAlign: 'start',
+          fontSize: '16px'
+        }}
+        variant="subtitle1">
+        {title}
+      </Typography>
+      <Box>{children}</Box>
+    </Paper>
+  );
+};
+
+export default Filter;
