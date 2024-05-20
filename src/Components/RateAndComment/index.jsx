909927fba8ca4d2fd8fbc688341c0d58a8935620
seyma <seyma.altiok@venhancer.com>
2024-05-20 11:02:48 +0300

    fix/rate component

--- src/Components/RateAndComment/index.jsx
+++ src/Components/RateAndComment/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dialog, DialogTitle, DialogContent, Grid, Stack, Typography } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, Grid, Stack } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import { useTranslation } from 'react-i18next';
 import Rating from '@mui/material/Rating';
@@ -9,6 +9,7 @@ import MainButton from '../Button/MainButton';
 import { useFormik } from 'formik';
 import { rateAndCommentSchema } from '../../Schemas';
 import LikeImage from '../../Image/thumbs-up_5223658.png';
+import ErrorPopup from '../ErrorPopup';
 const RateAndComment = ({ handleClose, open }) => {
   const { t } = useTranslation();
   const formik = useFormik({
@@ -22,72 +23,77 @@ const RateAndComment = ({ handleClose, open }) => {
     }
   });
 
-  const handleRateChange = (event, newValue) => {
-    formik.setFieldValue('rate', newValue);
-  };
-
-  const handleCommentChange = (event) => {
-    formik.setFieldValue('comment', event.target.value);
-  };
-
   return (
-    <Dialog
-      open={open}
-      onClose={handleClose}
-      sx={{
-        '& .MuiDialog-container': {
-          '& .MuiPaper-root': {
-            width: '100%',
-            maxWidth: '400px'
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        sx={{
+          '& .MuiDialog-container': {
+            '& .MuiPaper-root': {
+              width: '100%',
+              maxWidth: '400px'
+            }
           }
-        }
-      }}>
-      <DialogTitle>
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500]
-          }}>
-          <CloseIcon />
-        </IconButton>
-      </DialogTitle>
+        }}>
+        <DialogTitle>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500]
+            }}>
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
 
-      <DialogContent style={{ textAlign: 'center' }}>
-        <img src={LikeImage} style={{ width: ' 80px' }} />
-        <form onSubmit={formik.handleSubmit} noValidate>
-          <Stack direction="column" spacing={3}>
-            <Grid item xs={6}>
-              <Rating
-                name="simple-controlled"
-                value={formik.values.rate}
-                onChange={handleRateChange}
-                style={{ padding: '20px' }}
-              />
-            </Grid>
-
-            <Grid item xs={6}>
-              <Input
-                multiline
-                label={t('input.comment')}
-                rows={4}
-                type="text"
-                value={formik.values.comment}
-                onChange={handleCommentChange}
-                inputProps={{ maxLength: 1500 }}
-              />
-            </Grid>
-
-            <Grid item xs={12}>
-              <MainButton type="submit">{t('button.save')}</MainButton>
-            </Grid>
+        <DialogContent sx={{ textAlign: 'center' }}>
+          <Stack
+            sx={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginBottom: '20px'
+            }}>
+            <img src={LikeImage} style={{ width: ' 80px' }} />
           </Stack>
-        </form>
-      </DialogContent>
-    </Dialog>
+          <form onSubmit={formik.handleSubmit} noValidate>
+            <Stack direction="column" spacing={3}>
+              <Grid item xs={12}>
+                <Rating
+                  name="simple-controlled"
+                  value={formik.values.rate}
+                  onChange={formik.handleChange('rate')}
+                  required
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <Input
+                  multiline
+                  label={t('input.comment')}
+                  rows={4}
+                  type="text"
+                  value={formik.values.comment}
+                  onChange={formik.handleChange('comment')}
+                  inputProps={{ maxLength: 1500 }}
+                  error={formik.touched.comment && Boolean(formik.errors.comment)}
+                  onBlur={formik.handleBlur}
+                  required
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <MainButton type="submit">{t('button.save')}</MainButton>
+              </Grid>
+            </Stack>
+          </form>
+        </DialogContent>
+      </Dialog>
+      <ErrorPopup formik={formik} />
+    </>
   );
 };
 
